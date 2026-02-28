a7c124a4e5e64d053272ade6de8bf3233a6f744e
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-02-28 13:17:04 +0700

    feat: add handler for /index route

---
+++ internal/handlers/index.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/IhsanAlhakim/go-auth-api/internal/middlewares"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func (h *Handler) Index(w http.ResponseWriter, r *http.Request) {
+	userInfo := r.Context().Value(middlewares.ContextWithUserInfoKey).(jwt.MapClaims)
+	message := fmt.Sprintf("hello %s", userInfo["name"])
+	Response(w, P{Message: message}, http.StatusOK)
+}
